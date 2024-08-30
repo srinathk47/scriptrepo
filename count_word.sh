8520cd84b76b9700ea60bd1cf5f9271d24014ed8
srinathk47 <srinathkadiyala472@gmail.com>
2024-08-30 23:51:53 +0530

    update

--- count_word.sh
+++ count_word.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
+#!/bin/bash
+
 # Check if a file path is provided
 if [ -z "$1" ]; then
-  echo "Usage: $0 </scriptrepo>"
+  echo "Usage: $0 <file_path>"
   exit 1
 fi
 
 file_path="$1"
 
+# Check if the file exists
+if [ ! -f "$file_path" ]; then
+  echo "File not found!"
+  exit 1
+fi
 
-# sort the words, sort by count, and display the top 5
+# Read the file, convert to lowercase, remove non-alphanumeric characters, sort, count occurrences, and display the top 5
 cat "$file_path" | \
 tr '[:upper:]' '[:lower:]' | \
-tr -c '[:alnum:]' '\n' | \
+tr -c '[:alnum:]' '[\n*]' | \
 sort | \
 uniq -c | \
 sort -nr | \
