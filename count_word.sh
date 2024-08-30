1077e3e47676b01c4672f32d278ba8d3f9b5a694
srinathk47 <srinathkadiyala472@gmail.com>
2024-08-30 23:13:43 +0530

    i created  acount_task

---
+++ count_word.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Check if a file path is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <file_path>"
+  exit 1
+fi
+
+file_path="$1"
+
+
+# sort the words, sort by count, and display the top 5
+cat "$file_path" | \
+tr '[:upper:]' '[:lower:]' | \
+tr -c '[:alnum:]' '\n' | \
+sort | \
+uniq -c | \
+sort -nr | \
+head -n 5
