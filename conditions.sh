26f8824391a6931f7fb622946ca26b5567d1fe62
srinathk47 <srinathkadiyala472@gmail.com>
2024-08-31 23:30:30 +0530

    loopupdated

--- conditions.sh
+++ conditions.sh
@@ -2,11 +2,13 @@
 
 NUMBER=$2
 
-if [ $NUMBER -gt 20 ] #gt,lt , -ne
+if [ $NUMBER -gt20 ] #gt,lt , -ne
 then
+
 echo "given number: $NUMBER is greater than 20"
 
 else
-echo "given number: $NUMBER IS LES THAN 20"
+
+echo "given number: $NUMBER  is less than 20"
 
 fi
