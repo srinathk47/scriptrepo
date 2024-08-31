eecac5d03ea243426ab5b6acd61aab9a07f97eec
srinathk47 <srinathkadiyala472@gmail.com>
2024-08-31 22:53:10 +0530

    updated

--- spl-vars.sh
+++ spl-vars.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-echo "all variables passed to the script: $@"
+echo "all variables passed to the script:  $@"
 
-echo "Number of variables passed:$#"
+echo "Number of variables passed: $#"
 
-echo "script name:$0"
-echo "Current working directory:$PWD"
+echo "script name: $0"
+echo "Current working directory: $PWD"
+echo "Home directory of current user: $Home"
