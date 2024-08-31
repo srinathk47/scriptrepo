8edaf0a2fc5ba63cc245192b7fe0347ee7fea848
srinathk47 <srinathkadiyala472@gmail.com>
2024-08-31 23:00:39 +0530

    update

--- spl-vars.sh
+++ spl-vars.sh
@@ -7,3 +7,5 @@ echo "Number of variables passed: $#"
 echo "script name: $0"
 echo "Current working directory: $PWD"
 echo "Home directory of current user: $HOME"
+echo "PID of the script executing now is:$$"
+
